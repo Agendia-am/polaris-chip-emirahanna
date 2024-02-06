b3834fca6e25617637e0baabe48145086ca702a5
Emira Y <emirahanna@gmail.com>
2024-02-06 14:50:30 -0500

    Updated Card Visuals
    Made changes because I hated the blue I used. Updated it so that it follows conventions

--- src/my-card.js
+++ src/my-card.js
@@ -1,87 +1,104 @@
 import { LitElement, html, css } from "lit";
 
+//lol what is not working
+//the outline thats supposed to be there when it hovers over a card
+
+//TODO:
+//Because i hate myself i want to make it look like spotify
+
 export class MyCard extends LitElement {
   static get tag() {
-    return 'my-card';
+    return "my-card";
   }
 
   constructor() {
     super();
-    this.title = "This is a Brilliant Title";
+    this.title = "Title";
     this.link = "https://hax.psu.edu";
     this.description =
       "The picture above is a CSS meme about an airconditioner that was installed half inside the wall. In other words, the airconditioner has negative margins.";
-    this.image = "https://i.pinimg.com/originals/3d/8d/d8/3d8dd8fb5efdfd2ecedae9d47e1a1737.jpg";
+    this.image =
+      "https://i.pinimg.com/originals/3d/8d/d8/3d8dd8fb5efdfd2ecedae9d47e1a1737.jpg";
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
-      }
-      #cardlist {
-        float:left;
+        display: inline-flex;
+        --card-background-color: #0f0f0f;
+        --card-default-text-color: #f1f1f1;
+        flex-direction: row;
       }
 
       .card {
-        max-width: 400px;
+        width: 300px;
         height: auto;
         padding: 24px;
         margin: 8px;
-        border: 8px solid #07BEB8;
-        border-radius: 32px;
-        background-color: #68D8D6;
-        text-align: center;
-        transition: transform 0.3s all ease-in-out;
+        background-color: var(--card-background-color);
+        opacity: 0.8;
+        border-radius: 8px;
+        transition: 0.6s all ease-in-out;
       }
 
       .change-color {
         background-color: #add8e6;
       }
 
-      #btn {
-        background-color: #07BEB8;
-        color: white;
-        font-family: Noto Sans, sans-serif;
+      .btn {
+        background-color: #272727;
+        color: var(--card-default-text-color);
+        font-family: "Roboto", sans-serif;
         font-size: 16px;
-        padding: 16px 32px;
-        border-radius: 15px;
-        border-color: #07BEB8;
-        display: none;
+        padding: 16px;
+        border-radius: 32px;
+        border-color: #272727;
       }
 
-      .link{
+      .link {
         text-decoration: none;
       }
 
       .title {
         font-weight: bold;
-        font-family: 'Franklin Gothic Medium', sans-serif;
-        font-size: 32px;
-        color: #200116;
+        font-family: "Roboto", sans-serif;
+        font-size: 18px;
+        margin-left: 10px;
+        color: var(--card-default-text-color);
+        overflow: hidden;
+      
       }
 
       .text {
-        font-family: Verdana, sans-serif;
-        font-size: 16px;
-        color: #200116;
-        margin: 16px 64px;
-        align-content: justify;
-      }
-
-      #btn:focus,
-      #btn:hover {
-        background-color: #87F5FB;
+        font-family: "Roboto", sans-serif;
+        font-size: 14px;
+        overflow: hidden;
+        color: #aaaaaa;
+        margin-left: 10px;
+        overflow: hidden;
       }
 
       .pic {
-        width: 90%;
+        width: 300px;
+        height: 200px;
+        border-radius: 15px;
+        object-fit: cover;
       }
 
-      @media (min-width: 501px) and (max-width: 800px) {
-        #btn {
-          display: inline;
-        }
+      .img-container{
+        text-align: center;
+      }
+
+      .btn:focus,
+      .btn:hover {
+        background-color: #ff0000;
+      }
+
+      .card:hover,
+      .card:focus-within {
+        outline: 2px var(--card-background-color);
+        outline-offset: 16px;
+        opacity: 1;
       }
 
       @media (max-width: 500px) {
@@ -94,19 +111,22 @@ export class MyCard extends LitElement {
 
   render() {
     return html`
-      <div id = "cardlist">
-        <div class='card'>
-        <h1 class='title'>${this.title}</h1>
-          <div>
-              <img class='pic' src="${this.image}"
-              alt="CSS Meme relating to an airconditioner being too far into the wall"></img>
-              <p class='text'> ${this.description} </p>
-              <a class='link' href=${this.link}>
-                <button id='btn'>details</button>
-              </a>
-          </div>
+    <div class="card">
+      <div>
+        <div class="img-container"></h2>
+          <img
+            class="pic"
+            src="${this.image}"
+            alt="CSS Meme relating to an airconditioner being too far into the wall"
+          />
         </div>
-      </div>`;
+        <h2 class="title">${this.title}</h2>
+        <p class="text">${this.description}</p>
+        <a class="link" href=${this.link}>
+          <button class="btn">👍 | details</button>
+        </a>
+      </div>
+    </div>`;
   }
 
   static get properties() {
@@ -114,7 +134,7 @@ export class MyCard extends LitElement {
       title: { type: String },
       description: { type: String },
       link: { type: String },
-      image: { type: String },
+      image: { type: String, reflect: true },
     };
   }
 }
