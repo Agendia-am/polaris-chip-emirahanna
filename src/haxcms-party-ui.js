72fe0ebde4217c5c8792a750c789991f28aaa6b2
Emira Y <emirahanna@gmail.com>
2024-03-21 12:20:47 -0400

    Write haxcms-party-ui HTML

---
+++ src/haxcms-party-ui.js
@@ -0,0 +1,77 @@
+import "@lrnwebcomponents/rpg-character/rpg-character.js";
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import { html, css } from "lit";
+
+export class HaxcmsPartyUi extends DDD {
+  static get tag() {
+    return "haxcms-party-ui";
+  }
+
+  constructor(){
+    super();
+    /* idk how to get the user's handle so it's my username as a default for now */
+    /* TODO: can i make the array size change based on how many users i have? would need to create a new list every time we add it then right?*/
+    this.party = ["ezy5092"];
+  }
+
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        :host {
+          display: flex;
+        }
+        .container {
+          background-color: #f2f2f2;
+          padding: 20px;
+        }
+        .button-panel {
+        display: flex;
+        }
+
+      `,
+    ];
+  }
+
+  render() {
+    return html`
+      <div class="container">
+        <div class="button-panel">
+        <input type="text" class="search-input" placeholder="Search party member"/>
+          <button class="add-button">Add</button>
+          <button>Remove</button>
+          </div>
+    
+          <div>
+            <rpg-character walking seed=${this.party[0]}></rpg-character>
+            <rpg-character walking seed=${this.party[1]}></rpg-character>
+            <rpg-character walking seed=${this.party[2]}></rpg-character>
+            <rpg-character walking seed=${this.party[3]}></rpg-character>
+            <rpg-character walking seed=${this.party[4]}></rpg-character>
+          </div>
+          <button>Save Party Members</button>
+
+          </div>
+    `;
+  }
+
+  addItem(){
+    input = document.querySelector(".search-input").value;
+    this.item = {...this.item, item}
+
+
+  }
+
+  displayParty(){
+    /* map your party members here. return the full array of rpg characters with seeds */
+  }
+
+  static get properties() {
+    return {
+      party: { type: String, reflect: true},
+    };
+  }
+}
+
+globalThis.customElements.define(HaxcmsPartyUi.tag, HaxcmsPartyUi); 
+
