4ba0531d9aa6adc1097615159227b708aa67e464
Emira Y <emirahanna@gmail.com>
2024-03-24 06:34:11 -0400

    Update haxcms-party-ui.js

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -11,7 +11,7 @@ export class HaxcmsPartyUi extends DDD {
     super();
     /* idk how to get the user's handle so it's my username as a default for now */
     /* TODO: can i make the array size change based on how many users i have? would need to create a new list every time we add it then right?*/
-    this.party = [];
+    this.party = ["ezy5092", "","", "", "",  ];
   }
 
   static get styles() {
@@ -19,21 +19,28 @@ export class HaxcmsPartyUi extends DDD {
       super.styles,
       css`
         :host {
-          
           display: flex;
         }
         .container {
           background-color: #f2f2f2;
           padding: 20px;
+          
         }
         .button-panel {
           display: flex;
         }
 
-        button{
+        .party {
+          width: 500px;
+          height: 300px;
+          align-content: center;
+          color: black;
+        }
+
+        button {
           font-family: "Press Start 2P", system-ui;
-  font-weight: 400;
-  font-style: normal;
+          font-weight: 400;
+          font-style: normal;
         }
       `,
     ];
@@ -49,18 +56,21 @@ export class HaxcmsPartyUi extends DDD {
               class="search-input"
               placeholder="Search party member"
             />
-            <button class="add-button">Add</button>
+            <button class="add-button" @click="${this.updateContainer}">
+              Add
+            </button>
             <button class="remove-button">Remove</button>
           </div>
-
-          <div>
-            <rpg-character walking seed=${this.party[0]}></rpg-character>
-            <rpg-character walking seed=${this.party[1]}></rpg-character>
-            <rpg-character walking seed=${this.party[2]}></rpg-character>
-            <rpg-character walking seed=${this.party[3]}></rpg-character>
-            <rpg-character walking seed=${this.party[4]}></rpg-character>
-          </div>
-          <button class="save-button" @click="${this.makeItRain}">Save Party Members</button>
+          <div class="party">
+          <rpg-character seed=${this.party[0]}></rpg-character>
+          <rpg-character seed=${this.party[1]}></rpg-character>
+          <rpg-character seed=${this.party[2]}></rpg-character>
+          <rpg-character seed=${this.party[3]}></rpg-character>
+          <rpg-character seed=${this.party[4]}></rpg-character>
+        </div>
+          <button class="save-button" @click="${this.makeItRain}">
+            Save Party Members
+          </button>
         </div>
       </confetti-container>
     `;
@@ -68,14 +78,21 @@ export class HaxcmsPartyUi extends DDD {
 
   addItem() {
     input = document.querySelector(".search-input").value;
-    this.item = { ...this.item, item };
+    this.party = { ...this.party, item };
   }
 
-  displayParty() {
-    $
+  displayItem(item){
+    return html`<rpg-character seed="${item}"></rpg-character>`;  
+  }
 
-}
+  updateContainer() {
+    const container = this.shadowRoot.querySelector(".party");
+    this.party.forEach((item) => {
+      (this.displayItem(item));
+    });
+  }
 
+  
 
   makeItRain() {
     import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
@@ -90,6 +107,7 @@ export class HaxcmsPartyUi extends DDD {
   static get properties() {
     return {
       party: { type: String, reflect: true },
+      item: { type: String, reflect: true },
     };
   }
 }
